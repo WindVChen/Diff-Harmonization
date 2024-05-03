7d469e698924c27bf9f8e35d12c37296185e30b7
WindVChen <984958836@qq.com>
2024-05-03 22:57:18 +0800

    Fix CMD parameter passing bug.

--- thirdparty/edge_detector.py
+++ thirdparty/edge_detector.py
@@ -2,6 +2,7 @@ import kornia as kn
 import argparse
 from thirdparty.pidinet import models
 import torch
+from types import SimpleNamespace
 
 def get_edge(img):
     #img [1,3,512,512]
@@ -11,20 +12,23 @@ def get_edge(img):
     return img_edge #[1,1,512,512]
 
 '''PidiNet egge detector'''
-parser = argparse.ArgumentParser(description='PyTorch Pixel Difference Convolutional Networks')
-parser.add_argument('--pidinet_model', type=str, default='pidinet_tiny',
-        help='model to train the dataset')
-parser.add_argument('--sa', action='store_false',
-        help='use CSAM in pidinet')
-parser.add_argument('--dil', action='store_false',
-        help='use CDCM in pidinet')
-parser.add_argument('--config', type=str, default='carv4',
-        help='model configurations, please refer to models/config.py for possible configurations')
-parser.add_argument('--evaluate', type=str, default="thirdparty/pidinet/trained_models/table5_pidinet-tiny.pth",
-        help='full path to checkpoint to be evaluated')
+# parser = argparse.ArgumentParser(description='PyTorch Pixel Difference Convolutional Networks')
+# parser.add_argument('--pidinet_model', type=str, default='pidinet_tiny',
+#         help='model to train the dataset')
+# parser.add_argument('--sa', action='store_false',
+#         help='use CSAM in pidinet')
+# parser.add_argument('--dil', action='store_false',
+#         help='use CDCM in pidinet')
+# parser.add_argument('--config', type=str, default='carv4',
+#         help='model configurations, please refer to models/config.py for possible configurations')
+# parser.add_argument('--evaluate', type=str, default="thirdparty/pidinet/trained_models/table5_pidinet-tiny.pth",
+#         help='full path to checkpoint to be evaluated')
+#
+# parser.add_argument('--gpu', type=str, default='0', help='gpus available')
+# pidnet_args = parser.parse_args()
 
-parser.add_argument('--gpu', type=str, default='0', help='gpus available')
-pidnet_args = parser.parse_args()
+# create a namespace object instead of using parser.parse_args()
+pidnet_args = SimpleNamespace(config='carv4', dil=True, evaluate='thirdparty/pidinet/trained_models/table5_pidinet-tiny.pth', gpu='0', pidinet_model='pidinet_tiny', sa=True)
 
 
 def Initialize_PidNet(args):
